aabc8eb08288ff3635992606898e08e78c116765
KAwuss12 <kawamikolaj119@gmail.com>
2024-12-14 14:55:28 +0100

    fix allUsers bug

--- app/src/main/java/com/example/taskflow/addProject/APIViewModel.kt
+++ app/src/main/java/com/example/taskflow/addProject/APIViewModel.kt
@@ -1,10 +1,6 @@
 package com.example.taskflow.addProject
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.taskflow.addProject.users.User
--- app/src/main/java/com/example/taskflow/addProject/users/User.kt
+++ app/src/main/java/com/example/taskflow/addProject/users/User.kt
@@ -1,9 +1,10 @@
 package com.example.taskflow.addProject.users
 
+import com.google.gson.annotations.SerializedName
 import java.sql.Timestamp
 
 data class User(
-    val id: Int,
+    @SerializedName("user_id") val id: Int,
     val email: String,
     val username: String,
     val first_name: String,
